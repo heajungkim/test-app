623a385710a848436d3e34a8f753e5d7f2f25c68
amy_kim <amy_kim@naver.com>
2022-06-16 15:30:14 +0900

    vue

---
+++ src/App-textani.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <animated-number :value="value"  :duration="500"  :formatValue="formatToPrice" v-if="this.animateDisplayFlag" />
+  <br>
+  <input v-model="value" type="number" />
+  <br>
+  <br>
+  <button type="button" @click="test"> dkslfk숫자 애니메이션</button>
+  </div>
+</template>
+<script>
+import AnimatedNumber from "animated-number-vue";
+ 
+export default {
+  components: {
+    AnimatedNumber
+  },
+  data() {
+    return {
+      value: 0,
+      animateDisplayFlag: false
+    };
+  },
+  methods: {
+    formatToPrice(value) {
+      return `${Number(value).toFixed(0)}`;
+    },
+    test(){
+      this.animateDisplayFlag = true;
+    }
+  }
+};
+</script>
